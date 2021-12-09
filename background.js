f9eeca31680dddd050da4ef52ca7bf05bca6795d
man90 <man90@tuta.io>
2021-12-09 12:26:12 +0100

    Initialise connection to Discord API

--- background.js
+++ background.js
@@ -1,13 +1,109 @@
-chrome.storage.local.get(["discordToken"], (result) => {
-	console.log("Initial Discord token:", result.discordToken)
-})
+class Utils {
+	static getPlatformName() {
+		return navigator?.userAgentData?.platform || navigator?.platform || "Unknown"
+	}
 
-chrome.storage.onChanged.addListener((changes) => {
-	for (let [key, { newValue }] of Object.entries(changes)) {
-		if (key === "deezerData") {
-			console.log("New Deezer status:", newValue)
-		} else if (key === "discordToken") {
-			console.log("New Discord token:", newValue)
+	static getBrowserName() {
+		return navigator.brave ? "Brave" : "Chrome"
+	}
+
+	static getDeviceName() {
+		return "DeezerCord"
+	}
+}
+
+class DeezerCord {
+	static #heartbeatInterval = 41250
+	static #discordToken = null
+	static #deezerStatus = null
+	static #ws = null
+
+	static init() {
+		// Initialise Discord token if it was set in a previous session
+		chrome.storage.local.get(["discordToken"], (result) => {
+			DeezerCord.#setDiscordToken(result.discordToken)
+		})
+
+		// Watch for Discord token and Deezer status changes
+		chrome.storage.onChanged.addListener((changes) => {
+			for (let [key, { newValue }] of Object.entries(changes)) {
+				if (key === "deezerData") {
+					DeezerCord.#setDeezerStatus(newValue)
+				} else if (key === "discordToken") {
+					DeezerCord.#setDiscordToken(newValue)
+				}
+			}
+		})
+
+		// Initialise a websocket connection to Deezer
+		DeezerCord.#ws = new WebSocket("wss://gateway.discord.gg/?v=6&encoding=json")
+		DeezerCord.#ws.onopen = () => {
+			DeezerCord.#send(DeezerCord.#generateOpIdentifyPayload())
+		}
+		DeezerCord.#ws.onmessage = (e) => {
+			DeezerCord.#handleMessage(e.data)
 		}
 	}
-})
+
+	static #setDiscordToken(token) {
+		DeezerCord.#discordToken = token
+	}
+
+	static #setDeezerStatus(status) {
+		DeezerCord.#deezerStatus = status
+	}
+
+	static #setHeartbeatInterval(interval) {
+		DeezerCord.#heartbeatInterval = interval
+	}
+
+	static #send(payload) {
+		DeezerCord.#ws.send(JSON.stringify(payload))
+	}
+
+	static #handleMessage(message) {
+		const msg = JSON.parse(message)
+
+		if (msg.op === 10) {
+			DeezerCord.#setHeartbeatInterval(msg.d.heartbeat_interval)
+		} else {
+			console.log(msg)
+		}
+	}
+
+	static #generatePresenceStatus() {
+		const game = DeezerCord.#deezerStatus === null ? null : {
+			name:     "Deezer",
+			type:     2, // "Listening to"
+			details:  DeezerCord.#deezerStatus.song,
+			state:    "by " + DeezerCord.#deezerStatus.artist,
+			instance: false,
+		}
+
+		return {
+			game,
+			status: "online",
+			since: null,
+			afk: false,
+		}
+	}
+
+	static #generateOpIdentifyPayload() {
+		return {
+			op: 2, // "Identify"
+			d: {
+				token: DeezerCord.#discordToken,
+				properties: {
+					$os:      Utils.getPlatformName(),
+					$browser: Utils.getBrowserName(),
+					$device:  Utils.getDeviceName(),
+				},
+				compress: false,
+				large_threshold: 50,
+				presence: DeezerCord.#generatePresenceStatus(),
+			}
+		}
+	}
+}
+
+DeezerCord.init()
